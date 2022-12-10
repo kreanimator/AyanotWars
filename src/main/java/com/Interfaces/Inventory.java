dfcd0df44384eb1f2874f617df28cf7c2ed8eb14
kreanimator <backin.v@yandex.ru>
2022-12-10 19:27:08 +0200

    Added inventory interface.

--- src/main/java/com/Interfaces/Inventory.java
+++ src/main/java/com/Interfaces/Inventory.java
@@ -3,10 +3,22 @@ package com.Interfaces;
 import java.awt.event.ItemEvent;
 import java.util.ArrayList;
 import com.Items.*;
+import com.Tiles.*;
 
 public class Inventory {
     ArrayList<Item> items;
     public static final int TILE_SIZE = 50;
     public static final int ROWS = 9;
     public static final int COLUMNS = 9;
+
+    ArrayList<Patches> patches;
+    private ArrayList<Patches> fillPatches() {
+        ArrayList<Patches> patchesList = new ArrayList<>();
+        for (int i = 0; i < COLUMNS; i++) {
+            for (int j = 0; j < ROWS; j++) {
+                patchesList.add(new Patches(i, j));
+            }
+        }
+        return patchesList;
+    }
 }
