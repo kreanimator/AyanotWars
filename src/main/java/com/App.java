5478b5669d663e1c9a2d5d4b9c73dbb32cc0d4f1
kreanimator <backin.v@yandex.ru>
2022-12-15 23:29:58 +0200

    Added inventory interface. Realized inventory initialization by keyboard Added boss Solved some bugs Added drop algorithm Added players name Added name enter from the keyboard Added inventory close Added attack feature Added players move animation Realized attack method DONE Fixed attack method DONE Realized HP animation New interface Refactored code Attack animation Solved some bugs Made unbounded map Created dynamic action panel Changed interface, added new tiles Fixed turn initialization

--- src/main/java/com/App.java
+++ src/main/java/com/App.java
@@ -8,4 +8,10 @@ public class App {
     }
     // TODO: 07.12.2022 COLLISION
     // TODO: 10.12.2022 Checking Git Work
+    //TODO: 15.12.2022 Random generation of the road. Random generation of one tile after another.
+    //TODO: 15.12.2022 Enemies attack method. Attacking player and each other.
+    //TODO: 15.12.2022 Inheritance of playerclass to player generation.
+    //TODO: 15.12.2022 Realize enemy player chasing method.
+    //TODO: 15.12.2022 Realize drop & inventory.
+    //TODO: 15.12.2022 Realize spells.
 }
